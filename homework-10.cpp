6235a1a83258d86d623698caf8652f22e9c8c92b
zoxinc10061 <48340647+zoxinc10061@users.noreply.github.com>
2019-04-12 08:29:33 +0800

    Add files via upload

---
+++ homework-10.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    char input[100];
+    int i=0;
+    
+    printf("請輸入要轉換的字串:\n");
+    scanf("%s",&input[i]);
+	while((i<=100)&&(input[i]>='A'))
+	{
+    	if((input[i]>='A')&&(input[i]<='Z'))
+		{        
+    	    input[i]+=32;
+    	    printf("%c",input[i]);
+    	}
+		else if((input[i]>='a')&&(input[i]<='z'))
+		{
+    	    input[i]-=32;
+    	    printf("%c",input[i]);
+    	}
+    	i++;
+	}
+	printf("\n");
+    system("pause");
+	return 0;
+}
